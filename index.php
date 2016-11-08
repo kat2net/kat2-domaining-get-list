b92ccf5707097df6e1269e265d713bfb53d2ec73
Kat2 <main@kat2.net>
2016-11-09 07:00:34 +1100

    xx

--- index.php
+++ index.php
@@ -1,4 +1,11 @@
 <?php
-exec('php /app/bg.php > /dev/null &');
+$dir = $_SERVER['DOCUMENT_ROOT'];
+
+exec('sudo mkdir '.$dir.'/d/');
+exec('sudo touch '.$dir.'/d/lock');
+exec('sudo touch '.$dir.'/d/output');
+exec('sudo chmod 777 '.$dir.'/d/output')
+
+exec('php /app/bg.php > '.$dir.'/d/output &');
 
 echo 'k';
