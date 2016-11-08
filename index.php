7a19abf4d602b47a5035ed077a2feb9ff7c1a699
Kat2 <main@kat2.net>
2016-11-09 05:49:34 +1100

    x

--- index.php
+++ index.php
@@ -1,13 +1,13 @@
 <?php
 header('Access-Control-Allow-Origin: *');
-header('Content-Type: application/json');
 
-echo 'Begin ...<br />';
-for( $i = 0 ; $i < 10 ; $i++ )
-{
-    echo $i . '<br />';
-    flush();
-    ob_flush();
+ob_implicit_flush(1);
+
+for($i=0; $i<10; $i++){
+    echo $i;
+
+    //this is for the buffer achieve the minimum size in order to flush data
+    echo str_repeat(' ',1024*64);
+
     sleep(1);
 }
-echo 'End ...<br />';
