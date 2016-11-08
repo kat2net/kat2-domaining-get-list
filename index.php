ce2bde3bf3d79db5c6e8a697f4c3dfb96295b3f1
Kat2 <main@kat2.net>
2016-11-09 04:47:49 +1100

    x

--- index.php
+++ index.php
@@ -2,4 +2,36 @@
 header('Access-Control-Allow-Origin: *');
 header('Content-Typle: application/json');
 
-file_get_contents($_GET['url']);
+$list = file_get_contents($_GET['url']);
+$lines = explode("\n", $list);
+
+$domains = array();
+foreach($lines as $line){
+    $length = strlen($line);
+
+    if(($length > 5) && ($length < 15)){
+        if(strstr($line, '.')){
+            $tld = getTLD($line);
+
+            if(
+                ($tld == 'com')
+                ||
+                ($tld == 'org')
+                ||
+                ($tld == 'net')
+            ){
+                $domains[] = array(
+                    'domain' => $line,
+                    'tld' => $tld,
+                    'length' => $length
+                );
+            }
+        }
+    }
+}
+
+echo json_encode($domains, JSON_PRETTY_PRINT);
+
+function getTLD($domain){
+    return str_replace(explode('.', $domain)[0].'.', '', $domain);
+}
