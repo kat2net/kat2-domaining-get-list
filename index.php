480f38a28c949d6cf3bea3accbcf584c315f6aef
Kat2 <main@kat2.net>
2016-11-09 07:24:34 +1100

    xxxx

--- index.php
+++ index.php
@@ -1,8 +1,12 @@
 <?php
 $dir = $_SERVER['DOCUMENT_ROOT'];
 
+if(file_exists('/app/d/lock')){
+    echo 'locked';
+}else{
+    echo 'make lock';
 
+    file_put_contents('/app/d/lock', time());
 
-//exec('php /app/bg.php > '.$dir.'/d/output &');
-
-echo $dir;
+    //exec('php /app/bg.php > /app/d/output &');
+}
