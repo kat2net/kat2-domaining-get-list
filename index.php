c4153736fcb5b4bd2b88de0b0dd1ad915cea0d19
Kat2 <main@kat2.net>
2016-11-09 05:50:55 +1100

    x

--- index.php
+++ index.php
@@ -4,10 +4,10 @@ header('Access-Control-Allow-Origin: *');
 ob_implicit_flush(1);
 
 for($i=0; $i<10; $i++){
-    echo $i;
+    echo $i.'a';
 
     //this is for the buffer achieve the minimum size in order to flush data
-    echo str_repeat(' ',1024*64);
+    //echo str_repeat(' ',1024*64);
 
     sleep(1);
 }
