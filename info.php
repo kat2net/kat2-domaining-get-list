e763e457cd3c25b761f5895f84415a0957e7b805
Kat2 <main@kat2.net>
2016-11-09 07:49:30 +1100

    zxzzxc

--- info.php
+++ info.php
@@ -4,12 +4,24 @@ header('Content-Type: application/json');
 $worker_name = getenv('worker_name');
 
 $list_active = 'N/A';
+$list_domains_done = '';
+$list_domains_left = '';
+$list_percentage = '';
 
-$data = file('/app/d/output');
-$last_line = $data[count($data)-2];
+if(file_exists('/app/d/lock')){
+    $data = file('/app/d/output');
+    $last_line = $data[count($data)-2];
+    $last_line = explode('|', $last_line);
+    $last_line = explode(':', $last_line[0]);
 
-$last_line = explode('|', $last_line);
-$last_line = explode(':', $last_line[0]);
+    $list_active = 'yes';
+    $list_domains_done = $last_line[0];
+    $list_domains_left = $last_line[1];
+    $list_percentage = ($last_line[0] / $last_line) * 100;
+
+    $last_line = explode('|', $last_line);
+    $last_line = explode(':', $last_line[0]);
+}
 
 $array = array(
     'worker' => array(
@@ -17,12 +29,9 @@ $array = array(
     ),
     'list' => array(
         'active' => $list_active,
-        'domains_done' => $last_line[0],
-        'domains_left' => $last_line[1]
-        
-    ),
-    'output' => array(
-        'last_line' => $last_line
+        'domains_done' => $list_domains_done,
+        'domains_left' => $list_domains_left,
+        'percentage' => $list_percentage
     )
 );
 
